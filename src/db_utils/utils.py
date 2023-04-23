adf9f53b0637f3bf6f069cb1c2ec7084eb724164
mrYush <yuriy.simonov@tedo.ru>
2023-04-24 00:14:46 +0300

    docstrings

--- src/db_utils/utils.py
+++ src/db_utils/utils.py
@@ -1,6 +1,19 @@
+"""Utils for db_utils"""
 from typing import Dict, List
 
 
 def get_several_keys(item: Dict, keys: List[str]) -> dict:
-    """Get several pair key value from dict"""
+    """
+    Get several pair key value from dict
+    Parameters
+    ----------
+    item: Dict[Any, Any]
+        dict from which we want to get several keys
+    keys: List[Any]
+        list of keys wich need to be saved in new dict
+
+    Returns
+    -------
+        dict
+    """
     return {key: item[key] for key in keys}
