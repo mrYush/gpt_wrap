f2c8fbf3188b3fb4d387bf51fe7f1918e91550c2
mrYush <yuriy.simonov@tedo.ru>
2023-12-18 07:40:54 +0300

    upgrading

--- src/gpt_utils.py
+++ src/gpt_utils.py
@@ -1,46 +1,37 @@
 from typing import Dict, Optional
 
 import openai
+from openai import OpenAI
 
-from settings import MODEL_NAME, TEMPERATURE, MAX_TOKENS, OPENAI_TOKEN, \
-    MAX_TOKENS_CONTEXT_OUTPUT
+from settings import MODEL_NAME, TEMPERATURE, OPENAI_TOKEN, \
+    MAX_TOKENS_CONTEXT_OUTPUT, ORGANIZATION_ID
 
 openai.api_key = OPENAI_TOKEN
+client = OpenAI(
+    organization=ORGANIZATION_ID
+)
 
 
-def get_answer(prompt: Optional[str] = None,
-               messages: Optional[Dict[str, str]] = None) -> str:
-    if prompt is not None:
-        ai_kwargs = {
-            'model': "text-davinci-003",
-            'prompt': prompt,
-            'temperature': TEMPERATURE,
-            'max_tokens': MAX_TOKENS
-        }
-        api_resp = openai.Completion.create(
-            **ai_kwargs
-        )
-        response = api_resp['choices'][0]['text']
-    elif messages is not None:
-        ai_kwargs = {
-            'model': MODEL_NAME,
-            'messages': messages,
-            'temperature': TEMPERATURE,
-            'max_tokens': MAX_TOKENS_CONTEXT_OUTPUT
-        }
-        api_resp = openai.ChatCompletion.create(
-            **ai_kwargs
-        )
-        response = api_resp['choices'][0]['message']['content']
-    else:
-        raise KeyError("at least one must be provided prompt or messages")
+def get_answer(messages: Optional[Dict[str, str]] = None) -> str:
+    ai_kwargs = {
+        'model': MODEL_NAME,
+        'messages': messages,
+        'temperature': TEMPERATURE,
+        'max_tokens': MAX_TOKENS_CONTEXT_OUTPUT
+    }
+    api_resp = openai.ChatCompletion.create(
+        **ai_kwargs
+    )
+    response = api_resp['choices'][0]['message']['content']
     return response
 
 
 def get_gen_pic_url(text_description: str) -> str:
-    response = openai.Image.create(
+    response = client.images.generate(
+        model="dall-e-3",
         prompt=text_description,
+        size="1024x1024",
+        quality="standard",
         n=1,
-        size="1024x1024"
     )
     return response["data"][0]['url']
